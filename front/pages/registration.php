d5c75bff80987d5a91a17b22bb2d2806ae4ad19d
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-05-20 13:55:31 +0200

    feat : page registration

---
+++ front/pages/registration.php
@@ -0,0 +1,43 @@
+<section>
+    <img src="" alt="">
+    <h1>Créer un compte</h1>
+    <p>Rejoignez la maison Sakura Moon</p>
+    <form action="" method="get">
+        <div>
+            <label for="firstname">Prénom</label>
+            <input type="text" name="firstname" id="firstname" required />
+        </div>
+        <div>
+            <label for="name">Nom</label>
+            <input type="text" name="name" id="name" required />
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" required />
+        </div>
+        <div>
+            <div>
+                <label for="tel">Numéro de téléphone</label>
+                <input type="text" name="tel" id="tel" required />
+            </div>
+            <div>
+                <label for="birthday">Date de naissance</label>
+                <input type="number" name="birthday" id="birthday" required />
+            </div>
+        </div>
+        <div>
+            <label for="address">Adresse</label>
+            <input type="text" name="address" id="address" required />
+        </div>
+        <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password" required />
+        </div>
+        <div>
+            <label for="password">Confirmation du mot de passe</label>
+            <input type="password" name="password" id="password" required />
+        </div>
+        <input type="submit" value="CRÉER MON COMPTE" />
+    </form>
+
+</section>
