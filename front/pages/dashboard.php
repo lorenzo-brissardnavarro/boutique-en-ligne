ecf301142c8b34d4177da3948370bb11e327ebfe
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-05-20 19:20:12 +0200

    feat : page dashboard

---
+++ front/pages/dashboard.php
@@ -0,0 +1,57 @@
+<section>
+    <h1>Dashboard admin</h1>
+    <section>
+        <div>
+            <h2>Gestion des produits</h2>
+            <button>AJOUTER UN PRODUIT</button>
+        </div>
+
+        <div>
+            <article>
+                <div>
+                    <div>
+                        <img src="" alt="">
+                    </div>
+                    <div>
+                        <h3>Boucles d’oreilles poisson argent</h3>
+                        <p>Bijoux - <span>24.99 €</span></p>
+                        <p>5 en stock</p>
+                    </div>
+                </div>
+
+                <p>Élégantes et raffinées, ces boucles d’oreilles en forme de poisson Betta offrent un design délicat aux détails finement sculptés. Leur style poétique et artisanal apporte une touche unique et sophistiquée à toute tenue.</p>
+
+                <div>
+                    <button><i class="fa-solid fa-pen"></i></button>
+                    <button><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+            </article>
+        </div>
+
+    </section>
+
+    <section>
+        <div>
+            <h2>Gestion des catégories</h2>
+            <button>AJOUTER UNE CATÉGORIE</button>
+        </div>
+
+        <div>
+            <article>
+                <h2>BIJOUX</h2>
+                <div>
+                    <button><i class="fa-solid fa-pen"></i></button>
+                    <button><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+            </article>
+
+            <article>
+                <h2>ACCESSOIRES</h2>
+                <div>
+                    <button><i class="fa-solid fa-pen"></i></button>
+                    <button><i class="fa-solid fa-trash-can"></i></button>
+                </div>
+            </article>
+        </div>
+    </section>
+</section>
