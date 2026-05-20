922adc0a1c925dbe5fa313d324e26fbb6672b9ec
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-05-20 19:24:45 +0200

    feat : page 404 error

---
+++ front/pages/404.php
@@ -0,0 +1,15 @@
+<section>
+    <h1>Erreur 404</h1>
+
+    <h2>Oups... cette page s'est envolée</h2>
+    <h2> sous les fleurs de sakura.</h2>
+
+    <p>
+      La page que vous cherchez n'existe plus, ou n'a peut-être jamais existé.
+      Revenez à la boutique pour découvrir nos nouvelles créations.
+    </p>
+
+    <a href="">
+        <button>RETOUR À LA BOUTIQUE</button>
+    </a>
+</section>
