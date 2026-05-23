fad09b67721c655fa351ef3fee8146b6b105dce5
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-05-23 16:24:15 +0200

    style : login form page

--- front/views/login.php
+++ front/views/login.php
@@ -2,29 +2,43 @@
 require_once "layout/header.php";
 ?>
 
-<section>
-    <img src="" alt="">
-    <h1>Bon retour !</h1>
-    <p>Connectez-vous à votre compte</p>
-    <form action="" method="get">
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" required />
-        </div>
-        <div>
-            <label for="password">Mot de passe</label>
-            <div>
-               <input type="password" name="password" id="password" required />
-               <i class="fa-solid fa-eye-slash"></i>
-            </div>
-        </div>
-        <input type="submit" value="SE CONNECTER" />
-    </form>
-    <div>
-        <p>Vous n'avez pas de compte ?</p>
-        <a href="">S'inscrire</a>
-    </div>
+<section class="auth-page">
+    <div class="auth-page__card">
 
+        <div class="auth-page__logo">
+            <img src="../images/logo.png" alt="Logo Sakura Moon">
+        </div>
+
+        <h1 class="auth-page__title">Bon retour !</h1>
+        <p class="auth-page__subtitle">Connectez-vous à votre compte</p>
+
+        <form action="" method="get">
+
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required />
+            </div>
+
+            <div class="form-group input-password">
+                <label for="password">Mot de passe</label>
+
+                <input type="password" name="password" id="password" required />
+
+                <button type="button" class="input-password__toggle">
+                    <i class="fa-solid fa-eye-slash"></i>
+                </button>
+            </div>
+
+            <input type="submit" value="SE CONNECTER" class="input-button"/>
+
+        </form>
+
+        <div class="form-link">
+            <p>Vous n'avez pas de compte ?</p>
+            <a href="">S'inscrire</a>
+        </div>
+
+    </div>
 </section>
 
 <?php
