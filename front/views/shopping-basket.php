3393170f808c7cab82e9f065d687018aab7736fe
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-05-23 21:11:00 +0200

    style : user's shopping basket page

--- front/views/shopping-basket.php
+++ front/views/shopping-basket.php
@@ -2,65 +2,67 @@
 require_once "layout/header.php";
 ?>
 
-<section>
-    <h1>Mon panier</h1>
+<section class="container cart-page">
+    <h1 class="shop-page__title">Mon panier</h1>
+    <div class="cart-page__inner">
 
-    <div>
-      <section>
-        <article>
-          <div>
-            <div>
-                <img src="" alt="">
-            </div>
-            <div>
-                <p>Bijoux</p>
-                <h2>Boucles d’oreilles poisson argent</h2>
-                <p>24.99 €</p>
-            </div>
-          </div>
-          <div>
-            <div>
-                <button aria-label="Diminuer la quantité">
-                    <i class="fa-solid fa-minus"></i>
-                </button>
-                <span>1</span>
-                <button aria-label="Augmenter la quantité">
-                    <i class="fa-solid fa-plus"></i>
-                </button>
-            </div>
-            <button aria-label="Supprimer l’article">
-                <i class="fa-solid fa-trash-can"></i>
-            </button>
-          </div>
-        </article>
-      </section>
+        <section class="cart-items">
+            <article class="cart-item">
+                <div class="cart-item__info">
+                    <img src="../images/hero.png" alt="Boucles d’oreilles poisson argent" class="cart-item__img">
+                    <div>
+                        <p class="cart-item__badge">Bijoux</p>
+                        <h2 class="cart-item__name">Boucles d’oreilles poisson argent</h2>
+                        <p class="cart-item__price">24.99 €</p>
+                    </div>
+                </div>
+                <div class="cart-item__controls">
+                    <div class="product-detail__qty m0">
+                        <button aria-label="Diminuer la quantité">
+                            <i class="fa-solid fa-minus"></i>
+                        </button>
+                        <input type="number" value="1" min="1" aria-label="Quantité">
+                        <button aria-label="Augmenter la quantité">
+                            <i class="fa-solid fa-plus"></i>
+                        </button>
+                    </div>
 
-      <aside>
-        <h2>Récapitulatif</h2>
-        <div>
-            <div>
+                    <button class="cart-item__delete" aria-label="Supprimer l’article">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                </div>
+            </article>
+        </section>
+
+        <aside class="cart-summary">
+            <h2 class="cart-summary__title">Récapitulatif</h2>
+            <div class="cart-summary__line">
                 <span>Sous-total</span>
-                <span >40.98 €</span>
+                <span>40.98 €</span>
             </div>
-            <div>
+
+            <div class="cart-summary__line">
                 <span>Livraison</span>
                 <span>3.99 €</span>
             </div>
-            <p>Plus que 9.02 € pour la livraison offerte.</p>
-        </div>
-        <div>
-            <div>
+
+            <p class="cart-summary__note">Plus que 9.02 € pour la livraison offerte.</p>
+
+            <div class="cart-summary__line cart-summary__line--total">
                 <span>Total</span>
                 <span>44.97 €</span>
             </div>
 
-            <div>
+            <div class="cart-summary__checkout">
                 <button>VALIDER LA COMMANDE</button>
-                <button>CONTINUER MES ACHATS</button>
             </div>
-        </div>
-      </aside>
+
+            <button class="cart-summary__continue">CONTINUER MES ACHATS</button>
+
+        </aside>
+
     </div>
+
 </section>
 
 <?php
